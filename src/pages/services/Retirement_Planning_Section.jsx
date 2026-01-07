5158ac45d9f67f52e8f0081037ebb0e9639089a8
nishpaths <nishpatha09@gmail.com>
2026-01-07 16:39:15 -0700

    Remove Investments and Real Estate services from Retirement Planning section

--- src/pages/services/Retirement_Planning_Section.jsx
+++ src/pages/services/Retirement_Planning_Section.jsx
@@ -7,8 +7,6 @@ function Retirement_Planning_Section(){
         <h1 className="text-xl font-semibold font-cabin text-center mt-20 text-[#bab675]">Retirement Planning</h1>
             <section className="flex flex-wrap justify-center gap-4 px-20 pt-25 pb-20 min-h-screen">
                 <ServiceCard title="Retirement Planning" description="Prepare for retirement by saving and investing money to ensure a comfortable lifestyle after leaving the workforce." link="/services/retirement-planning" />
-                <ServiceCard title="Investments" description="Smart investing strategies to grow and preserve your wealth, aligned with your goals and risk tolerance." link="/services/investments" />
-                <ServiceCard title="Real Estate" description="Build wealth and diversify your financial portfolio through strategic real estate investments." link="/services/real-estate" />
             </section>
         </>
     );
